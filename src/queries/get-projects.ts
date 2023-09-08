bf2414b14aedf420338a4561210b1cc5ac9100b4
HighError <hierrorua@gmail.com>
2023-09-08 17:28:14 +0300

    Add pagination check for projects

--- src/queries/get-projects.ts
+++ src/queries/get-projects.ts
@@ -1,7 +1,7 @@
 export default function GetProjects(locale: string, page: number): string {
   return `
     query {
-        Projects ( locale : ${locale === 'en' ? '"en-US"' : '"uk-UA"'}, limit : 1, skip : ${
+        Projects ( locale : ${locale === 'en' ? '"en-US"' : '"uk-UA"'}, limit : 2, skip : ${
           page - 1
         } ) {
             items {
